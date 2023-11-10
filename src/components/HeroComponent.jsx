34b11df83245b73425d28a0352f3cf6bd18645c7
jrbyt3s <josee.ramirezi@gmail.com>
2023-11-10 10:19:19 -0500

    feature: add whatsapp link

--- src/components/HeroComponent.jsx
+++ src/components/HeroComponent.jsx
@@ -18,17 +18,17 @@ const HeroComponent = () => {
                 <p className="md:text-base text-sm font-semibold text-gray-400 mt-5">
                     {" "}
                     ¡Agenda una cita con nosotros!
-                    Visítanos y te brindaremos la mejor atención para el engreído del hogar.<br/>
-                    Recuerda que es importante que tu mascota acuda al veterinario esporádicamente por prevención y no sólo para tratar algún mal.<br/>
+                    Visítanos y te brindaremos la mejor atención para el engreído del hogar.<br />
+                    Recuerda que es importante que tu mascota acuda al veterinario esporádicamente por prevención y no sólo para tratar algún mal.<br />
                     ✓Encuéntranos en: Av. 13 de Enero, N°2201-2213 La Huayrona, San Juan de Lurigancho.
                     ✓Llámanos o escríbenos a nuestro número 966 842 258
                 </p>
                 <div className="mt-10 flex justify-center items-center gap-5">
                     <Link href='./citas' className="block text-white bg-orange-500 font-semibold rounded-full px-5 md:py-4 py-2 hover:bg-black hover:shadow-lg">Saca una cita</Link>
 
-                    <button className="bg-gray-50 rounded-full shadow-lg p-2">
+                    <Link href='https://api.whatsapp.com/send?phone=51966842258&text=Hola%2C%20sananavet%2C%0Adeseo%20agendar%20una%20cita%20para%20mi%20engreido' className="bg-gray-50 rounded-full shadow-lg p-2">
                         <AiOutlineWhatsApp className="text-[#25D366] md:text-5xl text-4xl hover:text-[#00A884] hover:shadow-lg" />
-                    </button>
+                    </Link>
                 </div>
             </div>
             {/* image area */}
