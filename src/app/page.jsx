6d94810ab02228b9306ff296d82476ed84e70cde
3X3L0N <josuejaureguicamacho@gmail.com>
2023-11-11 08:41:06 -0500

    last commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable jsx-a11y/alt-text */
-import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import imagecar from "public/images/Sannavet-Groomers-profesionales-2.jpg";
 
 function About() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
-
   return (
-    <div className="flex justify-between min-w-7xl my-24 md:flex-row flex-col gap-y-10">
-      <div className="mx-20">
-        <Image src={imagecar} width={1500} height={1500} objectFit="cover" />
+    <section className="flex justify-between min-w-7xl my-24 md:flex-row flex-col gap-y-10">
+      <div className="mr-20 mx-auto max-w-screen-xl md:block hidden">
+        <Image
+          src={imagecar}
+          width={1500}
+          height={1500}
+          objectFit="cover"
+          className="ml-4"
+        />
       </div>
       <div className="md:mr-20 mx-10 md:order-2 order-1">
         <h2 className="text-base text-orange-500 font-semibold">
@@ -32,24 +29,16 @@ function About() {
           avanzada para garantizar el diagnóstico preciso y el tratamiento
           eficaz. Nos enorgullece ser un lugar donde tanto los animales como sus
           dueños se sientan cómodos y bienvenidos.
-          <br/>
-          <br/>
+          <br />
+          <br />
           En Sannavet, nos enorgullecemos de ser tu centro de confianza para el
           cuidado integral de tus queridas mascotas. Con una ubicación
           privilegiada en el corazón de la ciudad, nuestra clínica ofrece una
           amplia gama de servicios especializados para asegurarnos de que tus
           peludos reciban la atención que merecen.
         </p>
-        <div className="mt-10 flex justify-center items-center gap-5">
-        <Link
-          href="./citas"
-          className="block text-white bg-orange-500 font-semibold rounded-full px-5 md:py-4 py-2 hover:bg-black hover:shadow-lg"
-          >
-          Saca una cita
-        </Link>
       </div>
-    </div>
-    </div >
+    </section>
   );
 }
 
