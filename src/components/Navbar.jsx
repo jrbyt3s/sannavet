d09d94a32e744f302684e5a90bf3f2915ee6d909
jrbyt3s <josee.ramirezi@gmail.com>
2023-11-15 22:10:39 -0500

    break code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,15 @@
-"use client";
-import { useState } from "react";
 import Link from "next/link";
+import React from "react";
 import Image from "next/image";
+import logoImage from "public/images/sannavet-logo-brand-removebg-resize.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiFillCloseCircle } from "react-icons/ai";
-import logoImage from "public/images/sannavet-logo-brand-removebg-resize.png";
 
 export default function Navbar() {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
   return (
     <>
       <header>
-        <nav className="flex justify-between fixed top-0 items-center w-full bg-slate-100 mx-auto h-[60px] z-50">
+        <nav className="flex justify-between fixed top-0 items-center w-full  bg-slate-100 mx-auto h-[60px] z-50">
           <div>
             <Image
               src={logoImage}
@@ -26,62 +19,39 @@ export default function Navbar() {
               className="w-[50%]"
             />
           </div>
-
-          <div
-            className={`${
-              isMobileMenuOpen
-                ? "absolute top-[60px] left-0 w-full"
-                : "md:static hidden md:block"
-            } bg-slate-100 min-h-[5vh] md:w-auto w-[100%] flex items-center px-5 md:flex-row flex-col gap-2 font-bold text-[#F29640]`}
-          >
-            <ul
-              className={`${
-                isMobileMenuOpen
-                  ? "flex flex-col md:flex-row md:items-center md:gap-[4vw] gap-2"
-                  : "flex flex-col items-center gap-2"
-              }`}
-            >
+          <div className="md:static absolute bg-slate-100 min-h-[5vh] left-0 top-[-200%] md:w-auto w-[100%] flex items-center px-5">
+            <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-2 font-bold text-[#F29640]">
               <li>
-                <Link href="/" className="hover:text-[#54AEF4]">
+                <Link className="hover:text-[#54AEF4]" href="./">
                   Inicio
                 </Link>
               </li>
               <li>
-                <Link href="/servicios" className="hover:text-[#54AEF4]">
+                <Link className="hover:text-[#54AEF4]" href="./servicios">
                   Servicios
                 </Link>
               </li>
               <li>
-                <Link href="/citas" className="hover:text-[#54AEF4]">
+                <Link className="hover:text-[#54AEF4]" href="./citas">
                   Citas
                 </Link>
               </li>
               <li>
-                <Link href="/ubicanos" className="hover:text-[#54AEF4]">
+                <Link className="hover:text-[#54AEF4]" href="./ubicanos">
                   Ubicanos
                 </Link>
               </li>
             </ul>
           </div>
-
           <div className="flex items-center gap-2">
             <Link
-              href="/login"
+              href="./login"
               className="block bg-orange-500 px-5 py-2 m-2 text-white font-semibold rounded-2xl hover:bg-[#54AEF4]"
             >
               Login
             </Link>
 
-            <button
-              onClick={toggleMobileMenu}
-              className="block w-6 h-6 mx-2 md:hidden focus:outline-none"
-            >
-              {isMobileMenuOpen ? (
-                <AiFillCloseCircle className="w-6 h-6" />
-              ) : (
-                <GiHamburgerMenu className="w-6 h-6" />
-              )}
-            </button>
+            <GiHamburgerMenu className="block w-6 h-6 mx-2 md:hidden" />
           </div>
         </nav>
       </header>
